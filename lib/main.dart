aa030783eedee5995d0cdcb22fa9cad65c2e584d
Lilyach <lilichf85@gmail.com>
2023-02-26 16:49:56 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter_project_navig/first_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Returned Pass Data',
+    home: FirstScreen(),
+  ));
+}
+    
